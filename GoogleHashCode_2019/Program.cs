7f05b6c6af700ff7451c852e7c0aa4018f5354f0
egger-maximilian <maximegger@gmail.com>
2019-03-01 11:45:54 +0100

    Initial check-in of module GoogleHashCode_2019

---
+++ GoogleHashCode_2019/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GoogleHashCode_2019
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
